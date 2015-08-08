022972c872378b52d2a0323066e73b33c2786ffb
sihendra@gmail.com <sihendra@gmail.com>
2015-08-08 08:10:28 +0700

    Add shipping file upload handler

--- woocommerce-warung-jne.php
+++ woocommerce-warung-jne.php
@@ -33,12 +33,14 @@ function warung_jne_init() {
          * @return void
          */
         public function __construct() {
-            $this->id                 = 'wc_warung_jne'; // Id for your shipping method. Should be uunique.
+            $this->id                 = 'warung_jne'; // Id for your shipping method. Should be uunique.
             $this->method_title       = __( 'JNE' );  // Title shown in admin
             $this->method_description = __( 'Pengiriman dengan JNE' ); // Description shown in admin
 
             $this->enabled            = "yes"; // This can be added as an setting but for this example its forced enabled
             $this->title              = "JNE"; // This can be added as an setting but for this example its forced.
+
+            $this->option_name_shipping_data = "woocommerce_warung_jne_shipping";
 
             $this->init();
         }
@@ -56,6 +58,8 @@ function warung_jne_init() {
 
             // Save settings in admin if you have any defined
             add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
+            // save file upload
+            add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_jne_options' ) );
         }
 
         /**
@@ -107,23 +111,63 @@ function warung_jne_init() {
                     'placeholder'	=> '0.00',
                     'default'		=> '1',
                 ),
-                'jne_service' => array(
-                    'type'          => 'jne_service',
-                ),
                 'jne_import' => array(
                     'type'          => 'jne_import',
-                ),
-                'jne_free_shipping' => array(
-                    'type'          => 'jne_free_shipping',
-                ),
-                'free_shipping_city' => array(
-                    'type'          => 'free_shipping_city',
-                ),
-                'jne_credit' => array(
-                    'type'          => 'jne_credit',
-                ),
+                )
             );
 
+        }
+
+        public function generate_jne_import_html()
+        {
+            ob_start();
+            $jne_data = get_option( $this->option_name_shipping_data );
+            ?>
+            <tr valign="top">
+                <th scope="row" class="titledesc"><?php _e('Impor Kota', 'woojne') ?> <img class="help_tip" data-tip="Masukan file csv untuk menginput data kota kamu." src="<?php echo plugins_url( 'images/help.png', __FILE__ ); ?>" height="16" width="16" /></th>
+                <td>
+                    <p><input type="file" name="woocommerce_warung_jne_import_city" id="woocommerce_warung_jne_import_city" style="min-width:393px;" /> <input name="save" class="button-primary help_tip" data-tip="Klik untuk melakukan upload data kota." class="button-primary" type="submit" value="Upload Data Kota"> <a href="http://www.agenwebsite.com/?add-to-cart=5318" class="button-primary help_tip" data-tip="Silahkan lengkapi form checkout untuk mendapatkan data kota." target="_blank">Download Data Kota</a>
+                    </p>
+                    <?php if(!empty($jne_data['cost_data'])) { ?>
+                        <p style="background: #FFF;  border-left: 4px solid #FFF;  -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.1);  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.1);  margin: 5px 0 2px;  padding: 5px 12px;  border-color: #7AD03A;">Anda telah melakukan upload data, dan jika Anda ingin <u>mengedit data kota</u>, Anda bisa mengedit file csv terlebih dahulu.</p>
+                    <?php } else { ?>
+                        <p style="background: #FFF;  border-left: 4px solid #FFF;  -webkit-box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.1);  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.1);  margin: 5px 0 2px;  padding: 5px 12px;  border-color: #D54E21;">Anda belum melakukan upload data kota.</p>
+                    <?php } ?>
+                </td>
+            </tr>
+            <?php
+            return ob_get_clean();
+        }
+
+        public function process_admin_jne_options()
+        {
+            if($_FILES['woocommerce_warung_jne_import_city']['error'] != UPLOAD_ERR_NO_FILE) {
+                $cities_cost = array();
+                $upload_path = $_FILES["woocommerce_warung_jne_import_city"]["tmp_name"];
+                $ext = strtolower(end(explode('.', $_FILES['woocommerce_warung_jne_import_city']['name'])));
+
+                if(!empty($upload_path) and $ext == 'csv') {
+                    $fd = fopen ($upload_path, "r");
+                    $city_counter = 0;
+                    while (!feof ($fd)) {
+                        $buffer = fgetcsv($fd, filesize( $upload_path ) );
+                        if(!empty($buffer[0])){
+                            $buffer[0] = $buffer[0];
+                            $buffer[1] = iconv( 'UTF-8', 'ISO-8859-15//TRANSLIT',$buffer[1]);
+
+                            $city_name = $buffer[0];
+                            $cities_cost[$city_name] = array( 'city' => $city_name, 'price' => $buffer[1] );
+                            $city_counter++;
+                        }
+                    }
+                    fclose ($fd);
+
+                    $jne_options['cost_data'] = $cities_cost;
+                    $jne_options['city_count'] = $city_counter;
+
+                    update_option( $this->option_name, $jne_options );
+                }
+            }
         }
 
 
